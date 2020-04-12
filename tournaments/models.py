7201794b63099903429c1a7c8a51c39a57c552fc
Étienne Beaulé <beauleetienne0@gmail.com>
2020-04-12 00:08:55 -0300

    Create simple DB models
    As a start to storing tab-site--Discord equivalencies, models for
    people and the tournament have been created, but without migrations.
    The models are based off of Tabbycat's schema.

--- tournaments/models.py
+++ tournaments/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Tournament(models.Model):
+	TABSITE_TABBYCAT = 'tc'
+	TABSITE_MIT = 'mit'
+
+	TABSITE_CHOICES = (
+		(TABSITE_TABBYCAT, 'Tabbycat'),
+		(TABSITE_MIT, 'MIT-Tab'),
+	)
+
+	name = models.CharField(max_length=150)
+	api = models.URLField()
+	tabsite = models.CharField(max_length=5, choices=TABSITE_CHOICES)
+
+
+class Team(models.Model):
+	tab_id = models.IntegerField()
+	tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+
+class Person(models.Model):
+	tab_id = models.IntegerField()
+	discord_name = models.CharField(max_length=35, null=True)
+
+
+class Speaker(Person):
+	team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+
+class Adjudicator(Person):
+	tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
